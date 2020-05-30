fa7a00fdff71ec9210e941ca5f2f04b2be4853a3
ap-- <andreas@poehlmann.io>
2020-05-30 13:18:07 +0200

    [app] make browserify work with open layers

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 
 import "@babel/polyfill";
 
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
 import $ from "jquery";
 
+require('./index.css')
+
 $(document).ready(() => {
-        console.log('ready...');
+    console.log('ready...');
+
+    const map = new Map({
+        target: 'earth-map',
+        layers: [
+            new TileLayer({
+                source: new OSM()
+            })
+        ],
+        view: new View({
+            center: [0, 0],
+            zoom: 0
+        })
+    });
 });
