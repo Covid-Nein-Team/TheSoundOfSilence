e6d471b789418cfc99c386992688c0a0b9be9e72
ap-- <andreas@poehlmann.io>
2020-05-31 15:07:57 +0200

    [data] fix accumulated data date bug

--- data/accumulate_data.py
+++ data/accumulate_data.py
@@ -1,10 +1,14 @@
 import csv
+import re
 
 # Accumulates all data in a shared CSV data file with the following columns:
 # date, country, dataset, value
 
 data_files = ['co_data.csv', 'covid_19_cases.csv', 'evi_data.csv', 'mobility_data.csv', 'no2_data.csv']
 measurement_names = ['Carbon Monoxide', 'COVID-19 cases', 'Enhanced Vegetation Index', 'Google Mobility Index', 'Nitrogen Dioxide']
+
+
+ymd = re.compile('20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]')
 
 out_data = []
 
@@ -19,7 +23,9 @@ for d in range(len(data_files)):
                 countries = row[1:]
                 l += 1
                 continue
-            date = row[0]
+            date = "{:d}-{:02d}-{:02d}".format(*map(int, row[0].split('-')))
+            if not ymd.match(date):
+                print("not matched", date)
             for c in range(len(countries)):
                 out_data.append([date, countries[c], measurement, row[c+1]])
 
